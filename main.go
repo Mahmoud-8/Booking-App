e936e71981ec12ca3d5649bdb2dfe9a0e18b2b57
Mahmoud-8 <me292949@gmail.com>
2024-08-14 19:37:26 +0300

    done slice

--- main.go
+++ main.go
@@ -4,16 +4,14 @@ import "fmt"
 
 func main() {
 
-	var conferenceName = "Go Conference"
+	 conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-	var bookings [50]string
+	 bookings := []string{}
 
 
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T", conferenceTickets, remainingTickets)
-
-
 	fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")															
 	fmt.Println("Get your tickets here to attend")
@@ -40,13 +38,10 @@ func main() {
 	fmt.Scan(&userTickets)
 	
 	remainingTickets = remainingTickets - userTickets
-	bookings[0] = firstName + " " + lastName
+	 bookings = append(bookings,  firstName + " " + lastName)
 
 
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 
 
 	
